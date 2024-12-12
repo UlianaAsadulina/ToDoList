a06cdb50c1725a9f5430fe75245a70c3a2502c60
UlianaAsadulina <u.asadulina@icloud.com>
2024-12-12 16:49:45 -0500

    deleteTask function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const tasksList = document.querySelector(".tasks");
 const addTask = document.getElementById ("addBtn");
 const taskText = document.getElementById("addTxt");
+const delTask = document.querySelector(".delete");
 
 
 
 
-
+//this function add one task to the tasks list
 function createTask() {
     let newTask = document.createElement("li") ;
     let text = taskText.value;
@@ -16,9 +17,16 @@ function createTask() {
         newTask.textContent = text;
         tasksList.appendChild(newTask); 
         taskText.value="";
-    }
-  
-
+    } 
 } 
 
-addTask.addEventListener('click', createTask) ;
+//this function delete one task from the tasks list
+function deleteTask(event) {
+    console.log(event.target);
+    let task = event.target.parentNode;
+    console.log(task);
+    tasksList.removeChild(task);
+}
+
+addTask.addEventListener('click', createTask);
+delTask.addEventListener('click', deleteTask);
