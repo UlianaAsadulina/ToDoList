ef23349aef56bf5496baf037844dc7220799f0c5
UlianaAsadulina <u.asadulina@icloud.com>
2024-12-13 10:29:07 -0500

    createButton function and multiple delete buttons listener

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const tasksList = document.querySelector(".tasks");
 const addTask = document.getElementById ("addBtn");
 const taskText = document.getElementById("addTxt");
-const delTask = document.querySelector(".delete");
+// const delTask = document.querySelector(".delete");
+const delTask = document.getElementsByClassName("delete");
 
 
 // this function updates the date and time
@@ -26,9 +27,31 @@ function createTask() {
     else {
         newTask.textContent = text;
         tasksList.appendChild(newTask); 
-        taskText.value="";
+        createButton (newTask, "important");
+        createButton (newTask, "edit"); 
+        createButton (newTask, "delete");       
+        clearInput();
     } 
 } 
+
+// reset empty input after task creation
+function clearInput() {
+    taskText.value="";
+}
+
+// this function adds one button to the new task
+function createButton(newTask, newBtnText) {
+    console.log(newBtnText);
+    let newButton = document.createElement("button");
+    newButton.textContent = newBtnText;
+    console.log(newButton.textContent);
+    newTask.appendChild(newButton);
+    newButton.setAttribute("class", newBtnText);
+    console.log(newTask);
+
+
+
+}
 
 //this function deletes one task from the tasks list
 function deleteTask(event) {
@@ -38,5 +61,14 @@ function deleteTask(event) {
     tasksList.removeChild(task);
 }
 
+// this function adds listener for every delete button
+function addListenerToDeleteButtons () {    
+    for (let i=0;i<delTask.length;i++){
+            delTask[i].addEventListener('click',deleteTask);
+            console.log(delTask[i]);
+    }
+}
+
 addTask.addEventListener('click', createTask);
-delTask.addEventListener('click', deleteTask);
+addListenerToDeleteButtons ();
+
