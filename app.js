d3b829e647f219e3292b5cf64e018804c15b5150
UlianaAsadulina <u.asadulina@icloud.com>
2024-12-15 17:02:52 -0500

    Edit button function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const checkbox = document.getElementsByClassName("taskCheck");
 const addTask = document.getElementById ("addBtn");
 const taskText = document.getElementById("addTxt");
 
+const editTask = document.querySelector(".edit");
 const delTask = document.getElementsByClassName("delete");
 const keyTask = document.getElementsByClassName("important");
 
@@ -118,15 +119,14 @@ function addListenerToDeleteButtons () {
 function finishTask(event) {
     let box = event.target;
     // If checked strikethrough
-                if (this.checked) {
-                    
-                    box.parentNode.setAttribute ("class", "boxChecked");
-                }
+        if (this.checked) {
+            box.parentNode.setAttribute ("class", "boxChecked");
+        }
 
-                // If unchecked, remove the strikethrough
-                else {
-                    box.parentNode.classList.replace ("boxChecked", "boxUnchecked");
-                }
+    // If unchecked, remove the strikethrough
+        else {
+            box.parentNode.classList.replace ("boxChecked", "boxUnchecked");
+        }
 }
 
 // add listener for every checkbox
@@ -138,6 +138,45 @@ function addListenerToCheck() {
 }
 
 
+// edit task
+function editTaskText (event) {   
+    let task = event.target.parentNode; // list element 
+    let child = task.childNodes; //all list children
+    console.log(child);
+    let text = child[1].innerText;
+  
+    
+    
+    // child[1].style.display="none";
+    console.log(child[1].style);
+    child[3].style.display="inline";
+    child[3].value = text;  
+    child[5].style.display='inline';
+    child[7].style.display="none";
+    child[9].style.display="none";
+    child[11].style.display="none";
+    // editSubmit(child);
+        
+}
+
+
+function editSubmit (event) {
+    let task = event.target.parentNode; // list element 
+    let child = task.childNodes; //all list children
+    console.log('after edit');
+    console.log(child);
+    let text = child[3].value; //edited value of input
+    child[1].innerText = child[3].value;
+    console.log(child[1].innerText);
+    child[1].style.display="inline";
+    child[1].childNodes.display = 'inline';
+    child[3].style.display="none";
+   
+    child[5].style.display='none';
+    child[7].style.display="inline";
+    child[9].style.display="inline";
+    child[11].style.display="inline";
+}
 
 addTask.addEventListener('click', createTask);
 taskText.addEventListener('keydown', enterPressed);
