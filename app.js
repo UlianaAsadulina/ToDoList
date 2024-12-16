b964e943b912d80258c0ba6e797c26130e24bb41
UlianaAsadulina <u.asadulina@icloud.com>
2024-12-15 23:29:09 -0500

    Check button /uncheck button

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const tasksList = document.querySelector(".tasks");
-const checkbox = document.getElementsByClassName("taskCheck");
+const checkbox = document.getElementsByClassName("boxUnchecked");
 
 const addTask = document.getElementById ("addBtn");
 const taskText = document.getElementById("addTxt");
 
-const editTask = document.querySelector(".edit");
 const delTask = document.getElementsByClassName("delete");
 const keyTask = document.getElementsByClassName("important");
 
-let isChecked = [];
-for (let i=0; i<tasksList.length; i++) {
-    isChecked[i]=false;
-}
+
 
 
 // set date and time
@@ -39,7 +35,9 @@ function createTask() {
         createButton (newTask, "important");
         createButton (newTask, "edit"); 
         createButton (newTask, "delete");     
-        addListenerToDeleteButtons ();  
+        addListenerToDeleteButtons (); 
+        addListenerToCheck ();
+        addListenerToImportantButtons () ;
         clearInput();
     } 
 } 
@@ -71,8 +69,8 @@ function createCheckBox (newTask, text) {
 
     let newCheckBox = document.createElement("input");
     newCheckBox.setAttribute("type", "checkbox");
-    newCheckBox.setAttribute("class", "taskCheck");
-    newLabel.prepend(newCheckBox);    
+    newCheckBox.setAttribute("class", "boxUnchecked");
+    newTask.prepend(newCheckBox);    
 
 }
 
@@ -116,17 +114,15 @@ function addListenerToDeleteButtons () {
 }
 
 // striketrough text then box checked
-function finishTask(event) {
-    let box = event.target;
-    // If checked strikethrough
-        if (this.checked) {
-            box.parentNode.setAttribute ("class", "boxChecked");
-        }
-
-    // If unchecked, remove the strikethrough
-        else {
-            box.parentNode.classList.replace ("boxChecked", "boxUnchecked");
-        }
+function finishTask(boxEvent){
+    console.trace(boxEvent.target.checked);
+    if (boxEvent.target.checked) {
+        boxEvent.target.nextElementSibling.setAttribute("class", "boxChecked");
+    }  else {
+        if( boxEvent.target.nextElementSibling 
+            && boxEvent.target.nextElementSibling.classList)
+            boxEvent.target.nextElementSibling.classList.replace ("boxChecked", "boxUnchecked");
+    }
 }
 
 // add listener for every checkbox
@@ -138,45 +134,6 @@ function addListenerToCheck() {
 }
 
 
-// edit task
-function editTaskText (event) {   
-    let task = event.target.parentNode; // list element 
-    let child = task.childNodes; //all list children
-    console.log(child);
-    let text = child[1].innerText;
-  
-    
-    
-    // child[1].style.display="none";
-    console.log(child[1].style);
-    child[3].style.display="inline";
-    child[3].value = text;  
-    child[5].style.display='inline';
-    child[7].style.display="none";
-    child[9].style.display="none";
-    child[11].style.display="none";
-    // editSubmit(child);
-        
-}
-
-
-function editSubmit (event) {
-    let task = event.target.parentNode; // list element 
-    let child = task.childNodes; //all list children
-    console.log('after edit');
-    console.log(child);
-    let text = child[3].value; //edited value of input
-    child[1].innerText = child[3].value;
-    console.log(child[1].innerText);
-    child[1].style.display="inline";
-    child[1].childNodes.display = 'inline';
-    child[3].style.display="none";
-   
-    child[5].style.display='none';
-    child[7].style.display="inline";
-    child[9].style.display="inline";
-    child[11].style.display="inline";
-}
 
 addTask.addEventListener('click', createTask);
 taskText.addEventListener('keydown', enterPressed);
